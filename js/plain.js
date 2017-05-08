7147066b5df1ecfad4a8e16f3b57e8b9326da0c5
briwa <shabriwa.shalat@gmail.com>
2017-05-09 00:17:37 +0800

    Even better path tweening approach

--- js/plain.js
+++ js/plain.js
@@ -57,7 +57,16 @@ BasicGame.Boot.prototype = {
     player.tint = 0x86bfda;
     player.anchor.set(0.5);
 
+    var getDir = function( prev, next ) {
+      if ( prev[0] == next[0] ) {
+        return prev[1] > next[1] ? 'up' : 'down';
+      } else if ( prev[1] == next[1] ) {
+        return prev[0] > next[0] ? 'left' : 'right';
+      }
+    };
+
     game.input.onDown.add(function() {
+      // todo: find out why cursorPosX / cursorPosY sometimes returns negative value
       var cursorPosX = Math.floor( cursorPos.x / 38 );
       var cursorPosY = Math.floor( cursorPos.y / 38 );
 
@@ -68,13 +77,54 @@ BasicGame.Boot.prototype = {
 
         path = finder.findPath( playerPos.x, playerPos.y, cursorPosX, cursorPosY, matrix );
 
-        var tweenArr = [];
-        for ( var i = 1; i < path.length; i++ ) {
+        if ( path.length <= 1 ) return;
+
+        var tweenedPath = path.reduce(function(acc, next) {
+          // always take the last path as the reference
+          var curr = acc[ acc.length - 1 ];
+
+          // first path ever, don't bother, mark them as init
+          if ( ! curr ) {
+            acc.push({
+              arr : next,
+              dir : 'init',
+              speed : 0
+            });
+
+            return acc;
+          }
+
+          // where is this next piece of path heading to?
+          var nextDir = getDir( curr.arr, next );
+
+          // current direction is the same with the next one
+          // meaning just add the speed, with updated x/y from the next
+          // update the direction as well in case the previous one was init
+          if ( curr.dir == 'init' || curr.dir == nextDir ) {
+            curr.arr[0] = next[0];
+            curr.arr[1] = next[1];
+            curr.dir = nextDir;
+            curr.speed++;
+          } else {
+            // this means it's making a turn
+            // and the turning 'move' itself has 1 speed, hence the initial speed
+            acc.push({
+              arr : next,
+              dir : nextDir,
+              speed : 1
+            });
+          }
+
+          return acc;
+        }, []);
+
+        const tweenArr = [];
+        for ( var i = 0; i < tweenedPath.length; i++ ) {
           var tween = game.add.tween( player );
           tween.to({
-            isoX : path[i][0] * 38,
-            isoY : path[i][1] * 38
-          }, 500, Phaser.Easing.Linear.None, false);
+            isoX : tweenedPath[i].arr[0] * 38,
+            isoY : tweenedPath[i].arr[1] * 38
+          }, tweenedPath[i].speed * 500, Phaser.Easing.Linear.None, false);
 
           if ( tweenArr.length ) {
             var lastTween = tweenArr[ tweenArr.length - 1 ];
